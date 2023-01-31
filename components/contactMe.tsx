f51f15328be24e19613cd349eac2d755edb42f5f
Rio <r.arswendo.r@gmail.com>
2023-01-31 23:19:03 +0700

    updated contact me to use icons and more links

--- components/contactMe.tsx
+++ components/contactMe.tsx
@@ -1,3 +1,4 @@
+import { Email, Instagram, LinkedIn, Twitter } from "@mui/icons-material";
 import { Box, Container, Grid, Typography } from "@mui/material";
 import Link from "next/link";
 
@@ -12,48 +13,33 @@ const ContactMe = () => {
         height: "100vh",
       }}
     >
-      {/* Change this to SVG Icon Later */}
       <Container>
         <Grid container spacing={1}>
           <Grid item xs={12}>
-            <Typography variant="h1" data-aos="fade-up">
+            <Typography variant="h1" data-aos="fade-down">
               Get in contact with me on:
             </Typography>
           </Grid>
-          <Grid item xs={4}>
+          <Grid item xs={4}></Grid>
+          <Grid item xs={1}>
+            <Link href="https://www.linkedin.com/in/rio-arswendo-rachmad-990a091a9/" >
+              <LinkedIn color='primary' fontSize='large' data-aos="zoom-in-up" data-aos-delay="200" />
+            </Link>
           </Grid>
           <Grid item xs={1}>
-            <Typography
-              variant="subtitle1"
-              data-aos="fade-up"
-              data-aos-delay="200"
-            >
-              <Link href="https://www.linkedin.com/in/rio-arswendo-rachmad-990a091a9/">
-                Linked In
-              </Link>
-            </Typography>
+            <Link href="https://www.instagram.com/notepad_pro/">
+              <Instagram color='primary' fontSize='large' data-aos="zoom-in-up" data-aos-delay="300" />
+            </Link>
           </Grid>
           <Grid item xs={1}>
-            <Typography
-              variant="subtitle1"
-              data-aos="fade-up"
-              data-aos-delay="300"
-            >
-              <Link href="https://www.linkedin.com/in/rio-arswendo-rachmad-990a091a9/">
-                Other Link
-              </Link>
-            </Typography>
+            <Link href="mailto:r.arswendo.r@gmail.com">
+              <Email color='primary' fontSize='large' data-aos="zoom-in-up" data-aos-delay="400" />
+            </Link>
           </Grid>
           <Grid item xs={1}>
-            <Typography
-              variant="subtitle1"
-              data-aos="fade-up"
-              data-aos-delay="300"
-            >
-              <Link href="https://www.linkedin.com/in/rio-arswendo-rachmad-990a091a9/">
-                Email
-              </Link>
-            </Typography>
+            <Link href="https://twitter.com/hyung_ji_">
+              <Twitter color='primary' fontSize='large' data-aos="zoom-in-up" data-aos-delay="500" />
+            </Link>
           </Grid>
         </Grid>
       </Container>

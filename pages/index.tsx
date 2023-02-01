98788a5ffc3cae244bf961e8473143a8b056f1a0
Rio <r.arswendo.r@gmail.com>
2023-02-01 16:29:43 +0700

    added title to webpage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import { Container } from "@mui/material";
+import Head from "next/head";
 import ContactMe from "../components/bio/contactMe";
 import Greeting from "../components/bio/greeting";
 import WhatIDo from "../components/bio/whatido";
@@ -7,6 +8,9 @@ import WorkHistory from "../components/bio/workHistory";
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Rio Arswendo Rachmad</title>
+      </Head>
       <Greeting />
       <WhatIDo />
       {/* Add My Work History And Projects Section */}

dfacfe7c59df20f7990f8ab3494910ab711565d2
Rio <r.arswendo.r@gmail.com>
2023-02-02 12:51:05 +0700

    refactored greeting

--- components/bio/greeting.tsx
+++ components/bio/greeting.tsx
@@ -1,3 +1,4 @@
+import { KeyboardArrowDown } from "@mui/icons-material";
 import { Box, Typography } from "@mui/material";
 
 const Greeting = () => {
@@ -16,12 +17,24 @@ const Greeting = () => {
       <Typography variant="h1" data-aos="fade-up" align='center'>
         Hi, I'm Rio.
       </Typography>
-      <Typography variant="h2" data-aos="fade-up" align='center'>
+      <Typography variant="h4" data-aos="fade-up" align='center'data-aos-delay="200">
         I love building awesome things with code.
       </Typography>
-      <Typography variant="subtitle1" data-aos="fade-up" data-aos-delay="500">
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <Typography variant="subtitle1" data-aos="fade-down" data-aos-delay="500">
         Scroll down to know me!
       </Typography>
+      <KeyboardArrowDown fontSize='large' data-aos="fade-up" data-aos-delay="700" />
     </Box>
   );
 };
